bcfb59bf320ffbfabccdf52e933842d969f72ff2
olivier briand <olivier.briand@orange.com>
2013-02-07 10:51:08 +0100

    Add menu items and actions when selected

--- src/com/obriand/android_actionbar/MainActivity.java
+++ src/com/obriand/android_actionbar/MainActivity.java
@@ -2,7 +2,10 @@ package com.obriand.android_actionbar;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,24 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+	    AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+	    switch (item.getItemId()) {
+	        case R.id.menu_settings:
+	            Log.i("actionbar:main:","settings");
+	            return true;
+	        case R.id.menu_secondactivity:
+	        	Log.i("actionbar:main:","secondactivity");
+	            return true;
+	        case R.id.menu_about:
+	        	Log.i("actionbar:main:","about");
+	            return true;
+	        default:
+	            return super.onContextItemSelected(item);
+	    }
 	}
 
 }
